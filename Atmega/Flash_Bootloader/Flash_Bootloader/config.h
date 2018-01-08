26d536dbbfcf907cf3e9edc658e7f3fbe9b05d4b
MemberM <markroters@gmx.de>
2018-01-08 22:03:50 +0100

    first check-in

---
+++ Atmega/Flash_Bootloader/Flash_Bootloader/config.h
@@ -0,0 +1,21 @@
+
+// ----------------------------------------------------------------------------
+// Bootloader Settings
+
+#define	BOOTLOADER_TYPE		0
+
+#define BOOTLOADER_BOARD_ID  0xff
+
+#define	BOOT_LED			B,4
+
+//#define	BOOT_INIT
+//#define	BOOT_LED_SET_OUTPUT
+//#define	BOOT_LED_ON
+//#define	BOOT_LED_OFF
+//#define	BOOT_LED_TOGGLE
+
+// ----------------------------------------------------------------------------
+// CAN Settings
+
+#define	MCP2515_CS			B,0
+#define	MCP2515_INT			D,2
